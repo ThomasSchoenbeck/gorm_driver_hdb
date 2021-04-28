d3d638eb226e4a788939b6d2197d2ab0752fd213
Revolve Yao <revolveyao@outlook.com>
2021-04-28 18:49:34 +0800

    fix

--- hdb.go
+++ hdb.go
@@ -201,18 +201,18 @@ func (dialector Dialector) BindVarTo(writer clause.Writer, stmt *gorm.Statement,
 }
 
 func (dialector Dialector) QuoteTo(writer clause.Writer, str string) {
-	writer.WriteByte('`')
+	writer.WriteByte('"')
 	if strings.Contains(str, ".") {
 		for idx, str := range strings.Split(str, ".") {
 			if idx > 0 {
-				writer.WriteString(".`")
+				writer.WriteString(".\"")
 			}
 			writer.WriteString(str)
-			writer.WriteByte('`')
+			writer.WriteByte('"')
 		}
 	} else {
 		writer.WriteString(str)
-		writer.WriteByte('`')
+		writer.WriteByte('"')
 	}
 }
 
